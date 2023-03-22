f7467cc2ed26fd2a8652bd17862705167a6e48f1
LuiiVasquez <luivasque95@gmail.com>
2023-03-22 12:13:58 -0600

    added test environment with happy-dom

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: { // <--- config de vitest
+    environment: 'happy-dom'
+  }
 })
